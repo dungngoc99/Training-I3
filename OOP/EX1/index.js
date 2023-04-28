14ed112279c7d08632d7bd020aa7fc17e71197e5
dungngoc99 <dung.trinhngoc.tnd@gmail.com>
2023-04-28 15:14:52 +0700

    update OOP-ex1

--- OOP/EX1/index.js
+++ OOP/EX1/index.js
@@ -1,30 +1,40 @@
-function Animal(spiece, name, classify, age, speed) {
-  this.spiece = spiece;
-  this.name = name;
-  this.classify = classify;
-  this.age = age;
-  this.speed = speed;
+class Animal {
+  constructor(spiece, name, classify, age, speed, calo, eat) {
+    this.spiece = spiece;
+    this.name = name;
+    this.classify = classify;
+    this.age = age;
+    this.speed = speed;
+    this.calo = calo;
+    this.eat = eat;
+  }
+  calories() {
+    return this.calo + this.eat;
+  }
+  realTimeRunning() {
+    if (this.calories() <= 10) {
+      return this.calories();
+    } else {
+      return 10;
+    }
+  }
+  calcDistance() {
+    if (this.age <= 40 && this.age >= 30) {
+      this.speed -= 1;
+      return this.speed * this.realTimeRunning();
+    }
+    if (this.age >= 41) {
+      this.speed -= 2;
+      return this.speed * this.realTimeRunning();
+    } else {
+      return this.speed * this.realTimeRunning();
+    }
+  }
 }
-
-Animal.prototype.calcDistance = function () {
-  let dis = 0;
-
-  if (this.age <= 40 && this.age >= 30) {
-    this.speed -= 1;
-    return (dis += this.speed * 10);
-  }
-  if (this.age >= 41) {
-    this.speed -= 2;
-    return (dis += this.speed * 10);
-  } else {
-    return (dis += this.speed * 10);
-  }
-};
-
-let micky = new Animal("dog", "Micky", "corgi", 20, 5);
-let milu = new Animal("dog", "Milu", "chihuahua", 35, 6);
-let john = new Animal("human", "John", "asian", 20, 7);
-let eto = new Animal("human", "Eto", "african", 50, 10);
+let micky = new Animal("dog", "Micky", "corgi", 20, 5, 0, 10);
+let milu = new Animal("dog", "Milu", "chihuahua", 35, 6, 0, 10);
+let john = new Animal("human", "John", "asian", 20, 7, 5, 5);
+let eto = new Animal("human", "Eto", "african", 50, 10, 5, 5);
 
 let arr = [micky, milu, john, eto];
 let max = arr[0].calcDistance();
