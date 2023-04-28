ab7e98f64ecbdd1bbc93a7bf875ccd2e08dfa46f
dungngoc99 <dung.trinhngoc.tnd@gmail.com>
2023-04-28 15:55:31 +0700

    update

--- OOP/EX1/index.js
+++ OOP/EX1/index.js
@@ -11,23 +11,23 @@ class Animal {
   calories() {
     return this.calo + this.eat;
   }
-  realTimeRunning() {
-    if (this.calories() <= 10) {
+  realTimeRunning(x) {
+    if (this.calories() <= x) {
       return this.calories();
     } else {
-      return 10;
+      return x;
     }
   }
-  calcDistance() {
+  calcDistance(x) {
     if (this.age <= 40 && this.age >= 30) {
       this.speed -= 1;
-      return this.speed * this.realTimeRunning();
+      return this.speed * this.realTimeRunning(x);
     }
     if (this.age >= 41) {
       this.speed -= 2;
-      return this.speed * this.realTimeRunning();
+      return this.speed * this.realTimeRunning(x);
     } else {
-      return this.speed * this.realTimeRunning();
+      return this.speed * this.realTimeRunning(x);
     }
   }
 }
@@ -36,13 +36,22 @@ let milu = new Animal("dog", "Milu", "chihuahua", 35, 6, 0, 10);
 let john = new Animal("human", "John", "asian", 20, 7, 5, 5);
 let eto = new Animal("human", "Eto", "african", 50, 10, 5, 5);
 
+let time = 10;
+
+console.log(micky.calcDistance(time));
+console.log(milu.calcDistance(time));
+console.log(john.calcDistance(time));
+console.log(eto.calcDistance(time));
+
 let arr = [micky, milu, john, eto];
-let max = arr[0].calcDistance();
+let max = arr[0].calcDistance(time);
 let max_index = 0;
-for (let i = 1; i < arr.length; i++) {
-  if (max < arr[i].calcDistance()) {
-    max = arr[i].calcDistance();
+for (let i = 0; i < arr.length; i++) {
+  if (max < arr[i].calcDistance(time)) {
+    max = arr[i].calcDistance(time);
     max_index = i;
   }
 }
+console.log(max);
+console.log(max_index);
 console.log("Doi tuong chay nhanh nhat la:", arr[max_index].name);
