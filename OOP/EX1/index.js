74638d97d09f01d5f52863ac77a48434615dd302
dung <dung.trinhngoc.tnd@gmail.com>
2023-05-11 11:15:00 +0700

    update oop1-2

--- OOP/EX1/index.js
+++ OOP/EX1/index.js
@@ -1,20 +1,30 @@
-let calo = 0;
+let calories = 0;
+let eat = 0;
 class Animal {
-  constructor(spiece, name, classify, age, speed, calo, eat) {
-    this.spiece = spiece;
+  constructor(name, age, spieces) {
     this.name = name;
-    this.classify = classify;
     this.age = age;
-    this.speed = speed;
-    this.calo = calo;
-    this.eat = eat;
+    this.spieces = spieces;
   }
-  calories() {
-    return this.calo + this.eat;
+}
+
+class Dog extends Animal {
+  constructor(name, age, spieces, eat, speed) {
+    super(name, age, spieces);
+    this.calories = eat;
+    this.speed = speed;
+  }
+  speed() {
+    if ((this.spieces = "corgi")) {
+      return 5;
+    }
+    if ((this.spieces = "chihuahua")) {
+      return 6;
+    }
   }
   realTimeRunning(time) {
-    if (this.calories() <= time) {
-      return this.calories();
+    if (this.calories < time) {
+      return this.calories;
     } else {
       return time;
     }
@@ -32,14 +42,52 @@ class Animal {
     }
   }
 }
-let micky = new Animal("dog", "Micky", "corgi", 20, 5, 0, 10);
-let milu = new Animal("dog", "Milu", "chihuahua", 35, 6, 0, 10);
-let john = new Animal("human", "John", "asian", 20, 7, 5, 5);
-let eto = new Animal("human", "Eto", "african", 50, 10, 5, 5);
+class Human extends Animal {
+  constructor(name, age, spieces, calories, eat, speed) {
+    super(name, age, spieces);
+    this.calories = calories;
+    this.eat = eat;
+    this.speed = speed;
+  }
+  speed() {
+    if ((this.spieces = "asian")) {
+      return 7;
+    }
+    if ((this.spieces = "african")) {
+      return 10;
+    }
+  }
+
+  realTimeRunning(time) {
+    this.calories += this.eat;
+    if (this.calories < time) {
+      return this.calories;
+    } else {
+      return time;
+    }
+  }
+  calcDistance(time) {
+    if (this.age <= 40 && this.age >= 30) {
+      this.speed -= 1;
+      return this.speed * this.realTimeRunning(time);
+    }
+    if (this.age >= 41) {
+      this.speed -= 2;
+      return this.speed * this.realTimeRunning(time);
+    } else {
+      return this.speed * this.realTimeRunning(time);
+    }
+  }
+}
+
+let micky = new Dog("Micky", 20, "corgi", 10, 5);
+let milu = new Dog("Milu", 35, "chihuahua", 10, 6);
+let john = new Human("John", 15, "asian", 5, 5, 7);
+let eto = new Human("Eto", 50, "african", 5, 5, 10);
 
 let time = 10;
 
-let arr = [micky, milu, john, eto];
+const arr = [micky, milu, john, eto];
 let max = arr[0].calcDistance(time);
 let max_index = 0;
 for (let i = 0; i < arr.length; i++) {
@@ -48,6 +96,5 @@ for (let i = 0; i < arr.length; i++) {
     max_index = i;
   }
 }
-console.log(max);
-console.log(max_index);
+
 console.log("Doi tuong chay nhanh nhat la:", arr[max_index].name);
