74b8b2a23ee5ed2f46779bcc4ad1f1114f026df8
Dang <Dang@DESKTOP-5C1ECOF>
2023-06-03 17:18:10 +0700

    acb

--- SQLExam.sql
+++ SQLExam.sql
@@ -1,20 +1,27 @@
 ﻿use Training
 go
 
-select * from dbo.Cart
-select * from dbo.CartDetail
-select * from dbo.Product
-select * from dbo.ProductCategory
+select * from Cart
+select * from CartDetail
+select * from Product
+select * from ProductCategory
 
 /*
 1. Truy vấn danh sách mặt hàng không disabled với các thông tin: 
 Id, Name, UnitName, CategoryId, CategoryName
 (select, where, join)
 */
-select pd.Id, pd.Name, pd.UnitName, pd.CategoryId, pc.Name as CategoryName
-from dbo.Product as pd 
-join dbo.ProductCategory as pc on pd.CategoryId = pc.Id
-where pd.IsDisabled = 0
+select  
+	p.Id as Id,  
+	p.Name as Name,  
+	p.UnitName as UnitName, 
+	p.CategoryId as CategoryId, 
+	pc.Name as CategoryName
+from 
+	Product p  
+	join ProductCategory pc on p.CategoryId = pc.Id
+where  
+	p.IsDisabled = 1
 
 
 /*
@@ -22,90 +29,117 @@ where pd.IsDisabled = 0
 CartId, ProductId, ProductName, CategoryName, Quantity, UnitName
 (select, join)
 */
-select Cart.Id, Product.Id, Product.Name, ProductCategory.Name, CartDetail.Quantity,Product.UnitName from dbo.CartDetail 
-join dbo.Cart on Cart.Id = CartDetail.CartId
-join dbo.Product on CartDetail.ProductId = Product.Id
-join dbo.ProductCategory on Product.CategoryId = ProductCategory.Id
+select
+	c.Id as CartId, 
+	p.Id as ProductId, 
+	p.Name as ProductName, 
+	pc.Name as CategoryName, 
+	cd.Quantity as Quantity,
+	p.UnitName as UnitName
+from 
+	CartDetail cd 
+	join Cart c on c.Id = cd.CartId
+	join Product p on cd.ProductId =  p.Id
+	join ProductCategory pc on  p. CategoryId = pc.Id
  
 /*
 3. Truy vấn danh sách các mặt hàng cùng với tổng số lượng mua trong tất cả cart, sắp xếp theo tổng số lượng giảm dần
 ProductId, ProductName, TotalQuantityInCart
 (select, join, group by, order by)
 */
-select Product.Id,Product.Name,CartDetail.Quantity as TotalQuantityInCart 
-from dbo.Cart 
-join dbo.CartDetail on Cart.Id = CartDetail.CartId
-join dbo.Product on CartDetail.ProductId = Product.Id
-order by TotalQuantityInCart desc
+select  
+	p.Id as ProductId,
+	p.Name as ProductName, 
+	cd.Quantity as TotalQuantityInCart 
+from 
+	Cart c 
+	join CartDetail cd on c.Id = cd.CartId
+	join Product p on cd.ProductId =  p. Id
+order by 
+	TotalQuantityInCart desc
 
 /*
 4. Truy vấn danh sách cart và tổng tiền của từng cart, sắp xếp theo số tiền giảm dần
 CartId, TotalPrice
 */
-select Cart.Id,SUM(Product.UnitPrice * CartDetail.Quantity) as TotalPrice
-from dbo.Cart 
-join dbo.CartDetail on Cart.Id = CartDetail.CartId
-join dbo.Product on CartDetail.ProductId = Product.Id
-group by Cart.Id
+select c.Id,SUM( p. UnitPrice * cd.Quantity) as TotalPrice
+from Cart c 
+join CartDetail cd on c.Id = cd.CartId
+join Product p on cd.ProductId =  p. Id
+group by c.Id
 order by TotalPrice desc
 
 /*
 5. Truy vấn danh sách mặt hàng cùng số lượng cao nhất có trong tất cả cart
 ProductId, ProductName, MaxQuantityInCart
 */
-select Product.Id as ProductId,Product.Name as ProductName, CartDetail.Quantity as MaxQuantityInCart
-from dbo.Product
-join dbo.CartDetail on CartDetail.ProductId = Product.Id
-where CartDetail.Quantity = 
-(
-	select max(CartDetail.Quantity) 
-	from dbo.CartDetail	
-)
-group by Product.Id,Product.Name,CartDetail.Quantity
+select  
+	p.Id as ProductId, 
+	p.Name as ProductName,
+	max(cd.Quantity) as MaxQuantityInCart
+from 
+	Product p
+	join CartDetail cd on cd.ProductId = p.Id
+group by  
+	p.Id, p.Name
 
 /*
 6. Truy vấn danh sách category cùng với số lượng sản phẩm, sắp xếp theo số lượng giảm dần
 CategoryId, CategoryName, NumberOfProduct
 */
 
-select ProductCategory.Id as CategoryId, ProductCategory.Name as CategoryName, Sum(CartDetail.Quantity) as NumberOfProduct
-from dbo.CartDetail  
-right join dbo.Product on CartDetail.ProductId= Product.Id
-right join dbo.ProductCategory on Product.CategoryId = ProductCategory.Id
-group by ProductCategory.Id, ProductCategory.Name
-order by NumberOfProduct desc
+select 
+	pc.Id as CategoryId, 
+	pc.Name as CategoryName,
+	coalesce(Sum(cd.Quantity),0) as NumberOfProduct
+from 
+	CartDetail cd  
+	right join Product p on cd.ProductId=  p. Id
+	right join ProductCategory pc on  p. CategoryId = pc. Id
+group by 
+	pc. Id, pc. Name
+order by 
+	NumberOfProduct desc
 
 
 /*
 7. Truy vấn danh sách category cùng tổng tiền hàng trong tất cả Cart, sắp xếp theo tiền hàng giảm dần
 CategoryId, CategoryName, TotalPrice
 */
-select ProductCategory.Id as CategoryId, 
-		ProductCategory.Name as CategoryName, 
-		SUM(CartDetail.Quantity*Product.UnitPrice) as TotalPrice
-from dbo.ProductCategory 
-left join dbo.Product on ProductCategory.Id = Product.CategoryId
-left join dbo.CartDetail on Product.Id = CartDetail.ProductId
-group by ProductCategory.Id, ProductCategory.Name
-order by TotalPrice desc
+select 
+	pc.Id as CategoryId, 
+	pc. Name as CategoryName, 
+	coalesce(SUM(cd.Quantity * p.UnitPrice),0) as TotalPrice
+from 
+	ProductCategory pc  
+	left join Product p on pc.Id =  p. CategoryId
+	left join CartDetail cd on  p.Id = cd.ProductId
+group by 
+	pc.Id, pc.Name
+order by 
+	TotalPrice desc
 
 
 /*
-8. Số lượng Cart phát sinh theo ngày
+8. Số lượng Cart c phát sinh theo ngày
 Date, NumberOfCart
 */
-select GETDATE() as Date,sum(CartDetail.Quantity) as NumberOfCart
-from dbo.CartDetail
-left join dbo.Cart on CartDetail.CartId = Cart.Id
-group by CartDetail.CartId
+select 
+	c.CreatedDate as Date,
+	sum(cd.Quantity) as NumberOfCart
+from 
+	CartDetail cd
+	left join Cart c on cd.CartId = c.Id
+group by 
+	c.CartId
 
 
 /*
-9. Tổng tiền trong Cart theo ngày
+9. Tổng tiền trong Cart c theo ngày
 Date, TotalPrice
 */
-select Cart.CreatedDate as Date, sum(CartDetail.Quantity*Product.UnitPrice ) as TotalPrice
-from dbo.CartDetail
-left join dbo.Cart on Cart.Id = CartDetail.CartId
-left join dbo.Product on CartDetail.ProductId = Product.Id
-group by Cart.CreatedDate
+select c.CreatedDate as Date, sum( cd.Quantity* p. UnitPrice ) as TotalPrice
+from CartDetail
+left join Cart c on c.Id = cd.CartId
+left join Product p on cd.ProductId =  p. Id
+group by c.CreatedDate
--- Training.sql
+++ Training.sql
@@ -1,20 +1,20 @@
 USE [Training]
 GO
-/****** Object:  Table [dbo].[Cart]    Script Date: 23/02/2023 1:14:05 PM ******/
+/****** Object:  Table [dbo].[Cart]    Script Date: 3/06/2023 11:13:21 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Cart](
-	[SessionId] [int] NOT NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[CreatedDate] [datetime] NOT NULL,
  CONSTRAINT [PK_Cart] PRIMARY KEY CLUSTERED 
 (
-	[SessionId] ASC
+	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[CartDetail]    Script Date: 23/02/2023 1:14:05 PM ******/
+/****** Object:  Table [dbo].[CartDetail]    Script Date: 3/06/2023 11:13:21 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -30,7 +30,7 @@ CREATE TABLE [dbo].[CartDetail](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Product]    Script Date: 23/02/2023 1:14:05 PM ******/
+/****** Object:  Table [dbo].[Product]    Script Date: 3/06/2023 11:13:21 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -48,7 +48,7 @@ CREATE TABLE [dbo].[Product](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[ProductCategory]    Script Date: 23/02/2023 1:14:05 PM ******/
+/****** Object:  Table [dbo].[ProductCategory]    Script Date: 3/06/2023 11:13:21 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -62,8 +62,50 @@ CREATE TABLE [dbo].[ProductCategory](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+SET IDENTITY_INSERT [dbo].[Cart] ON 
+GO
+INSERT [dbo].[Cart] ([Id], [CreatedDate]) VALUES (1, CAST(N'2023-02-27T16:24:57.160' AS DateTime))
+GO
+INSERT [dbo].[Cart] ([Id], [CreatedDate]) VALUES (2, CAST(N'2022-04-22T10:34:24.000' AS DateTime))
+GO
+INSERT [dbo].[Cart] ([Id], [CreatedDate]) VALUES (3, CAST(N'2022-04-25T00:00:00.000' AS DateTime))
+GO
+SET IDENTITY_INSERT [dbo].[Cart] OFF
+GO
+INSERT [dbo].[CartDetail] ([CartId], [ProductId], [Quantity]) VALUES (1, 10, 5)
+GO
+INSERT [dbo].[CartDetail] ([CartId], [ProductId], [Quantity]) VALUES (1, 20, 3)
+GO
+INSERT [dbo].[CartDetail] ([CartId], [ProductId], [Quantity]) VALUES (1, 22, 4)
+GO
+INSERT [dbo].[CartDetail] ([CartId], [ProductId], [Quantity]) VALUES (1, 30, 3)
+GO
+INSERT [dbo].[CartDetail] ([CartId], [ProductId], [Quantity]) VALUES (2, 10, 3)
+GO
+INSERT [dbo].[CartDetail] ([CartId], [ProductId], [Quantity]) VALUES (2, 21, 3)
+GO
+INSERT [dbo].[CartDetail] ([CartId], [ProductId], [Quantity]) VALUES (2, 22, 3)
+GO
+INSERT [dbo].[Product] ([Id], [Name], [UnitPrice], [UnitName], [IsDisabled], [CategoryId]) VALUES (10, N'Ipad Pro', 1000, N'Ipad Pro 2020', 1, 1)
+GO
+INSERT [dbo].[Product] ([Id], [Name], [UnitPrice], [UnitName], [IsDisabled], [CategoryId]) VALUES (20, N'Macbook Pro', 2000, N'Macbook  M1 2020', 1, 2)
+GO
+INSERT [dbo].[Product] ([Id], [Name], [UnitPrice], [UnitName], [IsDisabled], [CategoryId]) VALUES (21, N'Dell', 2000, N'Dell Ins', 1, 2)
+GO
+INSERT [dbo].[Product] ([Id], [Name], [UnitPrice], [UnitName], [IsDisabled], [CategoryId]) VALUES (22, N'Lenovo', 2000, N'Thinkpad', 1, 2)
+GO
+INSERT [dbo].[Product] ([Id], [Name], [UnitPrice], [UnitName], [IsDisabled], [CategoryId]) VALUES (30, N'Iphone', 999, N'Iphone 12', 1, 3)
+GO
+INSERT [dbo].[ProductCategory] ([Id], [Name]) VALUES (1, N'Tablet')
+GO
+INSERT [dbo].[ProductCategory] ([Id], [Name]) VALUES (2, N'Laptop')
+GO
+INSERT [dbo].[ProductCategory] ([Id], [Name]) VALUES (3, N'Phone')
+GO
+INSERT [dbo].[ProductCategory] ([Id], [Name]) VALUES (4, N'Desktop')
+GO
 ALTER TABLE [dbo].[CartDetail]  WITH CHECK ADD  CONSTRAINT [FK_CartDetail_Cart] FOREIGN KEY([CartId])
-REFERENCES [dbo].[Cart] ([SessionId])
+REFERENCES [dbo].[Cart] ([Id])
 GO
 ALTER TABLE [dbo].[CartDetail] CHECK CONSTRAINT [FK_CartDetail_Cart]
 GO
