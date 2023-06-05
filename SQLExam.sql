54c1302e718f318497e8e506dfe3a0877bad70cd
Dang <Dang@DESKTOP-5C1ECOF>
2023-06-05 08:26:12 +0700

    update

--- SQLExam.sql
+++ SQLExam.sql
@@ -21,7 +21,7 @@ from
 	Product p  
 	join ProductCategory pc on p.CategoryId = pc.Id
 where  
-	p.IsDisabled = 1
+	p.IsDisabled = 0
 
 
 /*
@@ -47,27 +47,31 @@ from
 ProductId, ProductName, TotalQuantityInCart
 (select, join, group by, order by)
 */
-select  
-	p.Id as ProductId,
-	p.Name as ProductName, 
-	cd.Quantity as TotalQuantityInCart 
+select 
+	p.Id,
+	p.Name,
+	coalesce(sum(cd.Quantity),0)
 from 
-	Cart c 
-	join CartDetail cd on c.Id = cd.CartId
-	join Product p on cd.ProductId =  p. Id
-order by 
-	TotalQuantityInCart desc
+	Product p
+	left join CartDetail cd on p.Id = cd.ProductId
+group by 
+	p.Id,p.Name
 
 /*
 4. Truy vấn danh sách cart và tổng tiền của từng cart, sắp xếp theo số tiền giảm dần
 CartId, TotalPrice
 */
-select c.Id,SUM( p. UnitPrice * cd.Quantity) as TotalPrice
-from Cart c 
-join CartDetail cd on c.Id = cd.CartId
-join Product p on cd.ProductId =  p. Id
-group by c.Id
-order by TotalPrice desc
+select 
+	c.Id as CartId,
+	SUM( p. UnitPrice * cd.Quantity) as TotalPrice
+from 
+	Cart c 
+	join CartDetail cd on c.Id = cd.CartId
+	join Product p on cd.ProductId =  p. Id
+group by 
+	c.Id
+order by 
+	TotalPrice desc
 
 /*
 5. Truy vấn danh sách mặt hàng cùng số lượng cao nhất có trong tất cả cart
@@ -91,12 +95,11 @@ CategoryId, CategoryName, NumberOfProduct
 select 
 	pc.Id as CategoryId, 
 	pc.Name as CategoryName,
-	coalesce(Sum(cd.Quantity),0) as NumberOfProduct
+	coalesce(count(p.ID),0) as NumberOfProduct
 from 
-	CartDetail cd  
-	right join Product p on cd.ProductId=  p. Id
-	right join ProductCategory pc on  p. CategoryId = pc. Id
-group by 
+	Product p
+	right join ProductCategory pc on p.CategoryId = pc.Id
+	group by 
 	pc. Id, pc. Name
 order by 
 	NumberOfProduct desc
@@ -121,25 +124,29 @@ order by
 
 
 /*
-8. Số lượng Cart c phát sinh theo ngày
+8. Số lượng Cart phát sinh theo ngày
 Date, NumberOfCart
 */
-select 
-	c.CreatedDate as Date,
-	sum(cd.Quantity) as NumberOfCart
-from 
-	CartDetail cd
-	left join Cart c on cd.CartId = c.Id
-group by 
-	c.CartId
 
+select 
+	CONVERT(date,c.CreatedDate) as Date,
+	COUNT(*) as NumberOfCart
+from 
+	Cart c
+group by 
+	CONVERT(date,c.CreatedDate)
 
 /*
-9. Tổng tiền trong Cart c theo ngày
+9. Tổng tiền trong Cart theo ngày
 Date, TotalPrice
 */
-select c.CreatedDate as Date, sum( cd.Quantity* p. UnitPrice ) as TotalPrice
-from CartDetail
-left join Cart c on c.Id = cd.CartId
-left join Product p on cd.ProductId =  p. Id
-group by c.CreatedDate
+
+select 
+	CONVERT(date,c.CreatedDate) as Date, 
+	coalesce(sum(p.UnitPrice*cd.Quantity),0) as TotalPrice
+from 
+	Cart c
+	left join CartDetail as cd on c.Id = cd.CartId
+	left join Product as p on cd.ProductId = p.Id
+group by 
+	CONVERT(date,c.CreatedDate)
